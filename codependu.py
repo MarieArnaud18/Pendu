d8d62178734c774f8cb51f16bed7283cc6901717
MarieArnaud18 <72148986+MarieArnaud18@users.noreply.github.com>
2020-12-10 10:53:14 +0100

    last commit

--- codependu.py
+++ codependu.py
@@ -1,37 +1,84 @@
-from  tkinter import *
-import random
+from tkinter import *
+from tkinter import messagebox
+from tkinter.messagebox import showinfo
+from tkinter.messagebox import showwarning
+from random import randint
+from tkinter import simpledialog
 
-Listemots = [] #initialistation de la liste de mots
-Listemots.append("maison")
-Listemots.append("table")
-Listemots.append("escargot")
-Listemots.append("fleur")
-Listemots.append("ordinateur")
+def Generationmot(fichiermots):
+    global Lmot,Ljoueur,chances,ldonnees,svj,svc,mot
+    fichier=open(fichiermots,'r')    #recuperation des mots du fichier
+    lignes=fichier.readlines()
+    fichier.close()  
+    
+    mot=randint(0,len(lignes)-1)     #choix aléatoire du mot
+    mot=lignes[mot]            
+    Lmot=[]                         #création de la liste contenant chaque lettre du mot
+    for i in mot:
+        Lmot.append(i)              
+    Lmot=Lmot[:-1]                  #enleve le /n
+    
+    Ljoueur=[Lmot[0]]               #création de la liste que l'on veut afficher pour le jeu
+    for i in range (1,len(Lmot)):
+        Ljoueur.append('_')
+    motjoueur=""
+    for i in Ljoueur:
+        motjoueur=motjoueur+i+" "
+        
+    ldonnees=[]                     #création de la liste des lettres déjà sasies par le joueur
 
-def ChoixMot(Listemots): #choix random d'un mot dans la liste
-    Mot=random.choice(Listemots) 
-    return Mot
+    chances=8
+      
+    svj.set(motjoueur)          #redonne un mot pour une nouvelle partie
+    Canevas.delete("all")       #efface le canevas avant chaque nouvelle partie 
+    Champ.delete(0,END)         #efface le champ de saisie avant chaque nouvelle partie
 
-def AffichageMot(Mot,LettresTrouvees):
-    a = ""
-    for i, L in enumerate(Mot): 
-        if i==0 or L in LettresTrouvees: #afficher la première lettre ainsi que les autres lettres trouvées
-            a+=L
-        else: #afficher _ pour les lettres non trouvees
-            a+=" _"
-    x.set(a)
-    return a
+def Pendu(): 
+    global Lmot,Ljoueur,chances,ldonnees
+    if not saisirlettre(): #appel de la fonction pour saisir la lettre
+        showwarning('Attention!','La lettre entrée a déjà été donnée.\nVeuillez recommencez!')    #creation d'une fenetre pop-up qui annonce que la lettre à deja eté donnée
+        return
+    ldonnees.append(lettre)
+    lettreinmot() #appel de la fonction pour tester si la lettre est dans le mot ou non
+    testfin() #appel de la fonction pour tester si le jeu est fini ou non
+    Affichage()
 
-def Gagne(Mot,LettresTrouvees):
-    i=0
-    for l in Mot:
-        if l in LettresTrouvees: #ajouter 1 au compteur si la lettre du mot est dans la liste de lettres trouvees
-            i+=1
-    if i==len(Mot): #si le nombre de lettres trouvees est égal à la longueur du mot retourner True
-        return True
+def saisirlettre():
+    global ldonnees,lettre
+    lettre=Lettre.get()      
+    return lettre not in ldonnees
+        
+def lettreinmot():
+    global Lmot,Ljoueur,chances,lettre
+    cpt=0
+    for i in range(1,len(Lmot)):  #test pour verifier si la lettre est dans le mot
+        if lettre==Lmot[i]:
+            cpt=1
+            Ljoueur[i]=lettre    #affiche la lettre à la place d'un - du mot
+    if cpt==0:
+        chances=chances-1
 
-def AffichagePendu():
-    global nbchance
+def testfin():
+    global Lmot,Ljoueur,chances,mot
+    test=True
+    if Lmot==Ljoueur:                   #le jeu s'arrete et l'utilisateur a trouvé toutes les lettres du mot
+        showinfo(mot,"Vous avez gagné!")  #affiche au joueur qu'il a ggné
+        test=False
+    if chances==-1:                   #le jeu s'arrete le joueur a utilisé toutes ses chances
+        showinfo(mot,"Vous avez perdu!")      #affiche au joueur qu'il a perdu  
+        test=False
+    if not(test):
+        if chances!=-1:
+            score()                 #propose au joeur d'entrer son nom d'utilisateur pour sauvegarder son score
+        rejouer()                   #propose au joueur de refaire une partie
+
+
+def Affichage():
+    global Ljoueur,chances,strvar
+    motjoueur=""
+    for i in Ljoueur:
+        motjoueur=motjoueur+i+" "
+    svj.set(motjoueur)
     if chances<=7:
         Canevas.create_line(20,180,120,180)      #creation du pendu qui augmente plus le nombre de chances diminue
     if chances<=6:
@@ -50,111 +97,67 @@ def AffichagePendu():
         Canevas.create_line(150,70,135,85)
         Canevas.create_line(150,70,165,85)
     Canevas.update()                   #rafraichit le canevas
+    
 
+def rejouer():
+    global svj
+    result = messagebox.askyesno("Rejouer", "Voulez-vous rejouer?", icon='info')   #propose au joueur de rejouer
+    if result:
+        svj.set("")               #recréation du mot avec que la 1ere lettre apparente pour permettre au joueur de rejouer
+        Generationmot('mots.txt') #
+    else:
+        Mafenetre.destroy()       #detruit la fenetre quand l'utilisateur ne veut pas jouer
+        
+def score():
+    global chances
+    bestscore=open('scores.txt','a+')
+    identifiant=simpledialog.askstring("Meilleurs Scores","Entrez votre identifiant")  #création d'une fenetre pop-up pour que le joueur rentre son identifiant
+    score=identifiant+":"+str(chances)+"\n"         #calcul du nb de points
+    bestscore.write(score)               #écrit les données sur le fichier
+    print('Le meilleur score est :',recuperer('scores.txt'))
 
-def AffichLettresFausses(LettresFausses):
-    A=""
-    for i in LettresFausses:
-        A+=i
-        A+=" "
-    List.set("Lettres fausses : "+A)
-    return A
+def recuperer(fichier):
+    fichier=open(fichier,'r')
+    lignes=fichier.readlines()
+    fichier.close()
+    best=0
+    for i in range(0,len(lignes)):
+        a,b=lignes[i].split(':')
+        b=int(b)
+        if b>best:
+            best=b
+    return best
+        
 
+#Generationmot('mots.txt')
 
-def Verif():
-    global LettresTrouvees,LettresFausses,Mot,nbchance
-    l=Lettre.get()
-    Lettre.set('')
-    if l in LettresTrouvees or l in LettresFausses: #si la lettre a déja été donnée
-        info.set("Vous avez déjà donné cette lettre")
-    elif l in Mot: #si la lettre est dans le mot
-        LettresTrouvees.append(l)#on ajoute la lettre à la liste de lettres trouvees
-        AffichageMot(Mot,LettresTrouvees)#afficher le mot avec la nouvelle lettre trouvee
-        info.set("Continuez, il manque encore des lettres")
-    else :
-        nbchance=nbchance-1 #enlever une chance à l'utilisateur
-        LettresFausses.append(l) #ajout de la lettre dans la liste de lettres fausses
-        AffichagePendu()
-        compt1.set("Nombre de coups restants: "+str(nbchance))
-        info.set("Aîe, c'est raté! Fais attention la prochaine fois")
-        AffichLettresFausses(LettresFausses) #On actualise la liste de lettres fausses
+global Lmot,Ljoueur,chances,ldonnees,lettre
 
+Mafenetre = Tk()           #creation de la fenetre principale
+Mafenetre.title("Jeu du pendu")
+L=200
+H=200
+Canevas=Canvas(Mafenetre, width=L, height=H)  #création d'un widget Canvas (zone graphique)
+Canevas.pack(padx=5,pady=5)
+svj = StringVar()
+svj.set("")
+Label1=Label(Mafenetre,textvariable=svj)         #creation d'un label pour afficher le mot (sous forme de tiret) du joueur
+Label1.pack(padx=10,pady=10)
 
-def Pendu():
-    global Mot,LettresTrouvees,LettresFausses,nbchance
-    AffichageMot(Mot,LettresTrouvees) #Affichage du mot avec les lettres trouvées
-    if nbchance>0:#tant que le joueur n'a pas épuisé ses chances
-        Verif()
-        if Gagne(Mot,LettresTrouvees)==True: #si l'utilisateur a donné toutes les bonnes lettres
-            info.set("Vous avez gagné ;-)")
-    if nbchance==0 and Gagne(Mot,LettresTrouvees)!=True : #si il manque des bonnes lettres et que le nombre de chance est épuisé
-        info.set("Vous avez perdu :-(")
+Proposer=Button(Mafenetre, text="Proposer", command= Pendu)
+Proposer.pack(side=LEFT, padx=10, pady=10)         #creation du bouton proposer qui lance le pendu
 
-def Rejouer():
-    global Mot,LettresTrouvees,LettresFausses,nbchance
-    Mot=ChoixMot(Listemots) #On choisit un nouveau mot
-    LettresTrouvees=[Mot[0]]#réinitialistation de la liste de lettres trouvees
-    LettresFausses=[]
-    nbchance = 8 #On redonne toutes les vies
-    Pendu()
-    return Mot,LettresTrouvees,LettresFausses,nbchance
+menubar=Menu(Mafenetre)
+menujeu=Menu(menubar,tearoff=0)
+menujeu.add_command(label="Recommencer",command= lambda:Generationmot('mots.txt'))
+menujeu.add_command(label="Quitter",command=Mafenetre.destroy)
+menubar.add_cascade(label="Jeu", menu=menujeu)     #creation du menu qui propose de recommencer ou quitter la partie
 
-
-#Création de la fenêtre principale
-Mafenetre=Tk()
-Mafenetre.title('jeu du pendu')
-
-#Création du Canvas
-Largeur=300
-Hauteur=300
-Canevas=Canvas(Mafenetre, height= Hauteur, width=Largeur,bg='white')
-
-#Création entry
 Lettre=StringVar()
-BoutonEntry=Entry(Mafenetre,textvariable=Lettre)
+Champ=Entry(Mafenetre,textvariable=Lettre) #création du champ de saisie pour rentrer les lettres
+Champ.focus_set()
+Champ.pack(side=LEFT,padx=10,pady=10)
 
-#Création du bouton proposer
-BoutonProposer=Button(Mafenetre,text='Proposer',command = Pendu)
-
-#Création du bouton rejouer
-BoutonRejouer=Button(Mafenetre,text='Rejouer',command=Rejouer)
-
-#Création bouton fermer
-BoutonQuitter=Button(Mafenetre,text='Quitter',command=Mafenetre.destroy)
-
-#Création label du mot recherché
-x=StringVar()
-x.set(AffichageMot(Mot,LettresTrouvees))
-LabelMotRech=Label(Mafenetre,textvariable=x,fg='black',bg='white')
-
-#Création label nb de coups restants
-compt1=StringVar()
-compt1.set("Nombre de coups restants: "+str(nbchance))
-LabelCoup=Label(Mafenetre,textvariable=compt1,fg='black',bg='white')
-
-#Création label lettres fausses
-List=StringVar()
-LabelLettresFausses=Label(Mafenetre,textvariable=List,fg='black',bg='white')
-
-info=StringVar()
-console=Label(Mafenetre, textvariable=info, fg='black', bg='white')
-
-#Mise en page
-LabelCoup.grid(row=1,sticky=NE)
-LabelMotRech.grid(row=2)
-BoutonEntry.grid(row=3)
-BoutonProposer.grid(row=4)
-BoutonRejouer.grid(row=5)
-BoutonQuitter.grid(row=6)
-LabelLettresFausses.grid(row=7)
-Canevas.grid(row=1,column=2,rowspan=6)
-console.grid(row=7, column=2)
-
-Rejouer()
-
-Mot=Rejouer()[0]
-LettresTrouvees=Rejouer()[1]
-LettresFausses=Rejouer()[2]
-nbchance=Rejouer()[3]
-
+Mafenetre.config(menu=menubar)   #affichage du menu
+Generationmot('mots.txt')       
 Mafenetre.mainloop()
